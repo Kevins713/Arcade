3aa5bc94e493b2af1e5b46c7fd174a58c3f13ca9
Kevins713 <68874726+Kevins713@users.noreply.github.com>
2021-06-17 11:03:49 +0200

    Ajout du slug dans l'entité Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -29,6 +29,11 @@ class Category
      */
     private $subCategories;
 
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->subCategories = new ArrayCollection();
@@ -77,6 +82,18 @@ class Category
                 $subCategory->setCategory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
