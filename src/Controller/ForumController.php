60b3dcca1e0b93b6d653f86b2bb1ce78479fbf18
Shil Nailo <a@a.a>
2021-06-18 12:24:31 +0200

    ajout et config de paginator

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ForumController extends AbstractController
 {
