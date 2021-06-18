533b04fa070b6dfaf4fa364b6c5e80e5bf998874
Shil Nailo <a@a.a>
2021-06-18 13:05:30 +0200

    création hierarchie de base des routes category/sub-category/forum + rename fichier forum

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -5,19 +5,46 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Component\HttpFoundation\Request;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use App\Entity\SubCategory;
+use App\Entity\Category;
+use App\Entity\Forum;
+use App\Entity\Comment;
+
+use \DateTime;
+
+/**
+ * @Route("/category", name="category")
+ */
 class ForumController extends AbstractController
 {
     /**
-     * @Route("/forum", name="forum")
+     * @Route("/", name="category")
      */
-    public function index(): Response
+    public function category(): Response
     {
-        return $this->render('forum/index.html.twig', [
-            'controller_name' => 'ForumController',
-        ]);
+        return $this->render('forum/category.html.twig'
+        );
     }
+    /**
+     * @Route("/sub-category/", name="sub_category")
+     */
+    public function sub_category(): Response
+    {
+        return $this->render('forum/subCategory.html.twig'
+        );
+    }
+    /**
+     * @Route("/sub-category/forum/", name="forum")
+     */
+    public function forum(): Response
+    {
+        return $this->render('forum/forum.html.twig');
+    }
+
 }
