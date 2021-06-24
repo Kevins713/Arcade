30bf23f3a1ec41b1921ac082f93f905406c7a026
TatsuyaSapphire <abc@a.c>
2021-06-24 11:09:48 +0200

    ajout création forum et redirection des pages

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -11,10 +11,13 @@ use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\CategoryRepository;
+use App\Repository\SubCategoryRepository;
 use App\Repository\ForumRepository;
 
 use App\Form\CreateCommentFormType;
+use App\Form\SubCategoryFormType;
 use App\Form\CategoryFormType;
+use App\Form\ForumFormType;
 
 use App\Entity\Category;
 use App\Entity\SubCategory;
@@ -27,7 +30,7 @@ use \DateTime;
 /**
  * Contrôleurs de la page qui liste les categories du site
  *
- * @Route("/categorie", name="category_")
+ *
  */
 class ForumController extends AbstractController
 {
@@ -82,13 +85,24 @@ class ForumController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("categorie/{slug}/", name="category")
+     */
+    public function category(SubCategoryRepository $subCategory ,Category $category, Request $request): Response
+    {
 
+        return $this->render('forum/category/category.html.twig',[
+            'categorie' => $category,
+        'subcategories' => $subCategory->findAll(),
+        ]);
+
+    }
 
 
     /**
      * Contrôleur de la page permettant de créer une nouvelle sous categorie
      *
-     * @Route("/{slug}/nouvelle-souscategorie/", name="new_subcategory")
+     * @Route("/nouvelle-souscategorie/{slug}", name="new_subcategory")
      * @Security("is_granted('ROLE_ADMIN','ROLE_MODERATOR')")
      */
     public function newSubCategory(Request $request, Category $category): Response
@@ -128,7 +142,7 @@ class ForumController extends AbstractController
             );
 
             $this->addFlash('success', 'Sous-Catégorie créée avec succès !');
-            return $this->redirectToRoute('category_sub_category',[
+            return $this->redirectToRoute('category',[
                 'slug'=> $category->getSlug()
             ]);
         }
@@ -139,77 +153,56 @@ class ForumController extends AbstractController
     }
 
         /**
-     * Contrôleur de la page permettant de créer un nouveau forum
-     *
-     * @Route("/{slug}/nouveau-forum/", name="new_forum")
-     * @Security("is_granted('ROLE_ADMIN','ROLE_MODERATOR')")
+     * @Route("/forumlist/{slug}", name="forumlist")
      */
-    public function newForum(Request $request, Forum $forum): Response
+    public function forumList(Request $request, SubCategory $subCategory): Response
     {
 
+        return $this->render('forum/forumList.html.twig',[
+            'subcategory' => $subCategory,
+        ]);
 
+    }
+
+
+    /**
+     * Contrôleur de la page permettant de créer un nouveau forum
+     *
+     * @Route("/nouveau-forum/{slug}", name="new_forum")
+     * @Security("is_granted('ROLE_ADMIN','ROLE_MODERATOR')")
+     */
+    public function newForum(Request $request, SubCategory $subCategory): Response
+    {
         $newForum = new Forum();
         $form = $this->createForm(ForumFormType::class, $newForum);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            $image = $form->get('image')->getData();
-
-            $imageDirectory = $this->getParameter('app_category_image_directory');
             $connectedUser = $this->getUser();
-
-            do {
-
-                $newFileName = md5($connectedUser->getId() . random_bytes(100)) . '.' . $image->guessExtension();
-
-                dump($newFileName);
-
-            } while (file_exists($imageDirectory . $newFileName));
-
-            // Mise à jour du nom de la photo de la sous catégorie
-            $newForum->setImage($newFileName);
-            $newForum->setCategory($forum);
+            $newForum
+            ->setSubCategory($subCategory)
+            ->setAuthor($connectedUser)
+            ->setPublicationDate(new DateTime());
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($newForum);
             $em->flush();
 
-            $image->move(
-                $imageDirectory,
-                $newFileName
-            );
+
 
             $this->addFlash('success', 'Forum créée avec succès !');
-            return $this->redirectToRoute('category_forumlist',[
-                'slug'=> $category->getSlug()
+            return $this->redirectToRoute('forumlist',[
+                'slug'=> $subCategory->getSlug(),
             ]);
         }
 
-        return $this->render('forum/category/newForum.html.twig', [
+        return $this->render('forum/newForum.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
-
-
     /**
-     * @Route("/sous-categorie/", name="sub_category")
-     */
-    public function subCategory(SubCategoryRepository $subCategory,Category $category, Request $request): Response
-    {
-
-        return $this->render('forum/category/subCategory.html.twig',[
-            'categorie' => $category,
-        'subcategories' => $subCategory->findAll(),
-        ]);
-
-    }
-
-
-
-    /**
-     * @Route("/sous-categorie/{slug}/", name="forum")
+     * @Route("/forum/{slug}/", name="forum")
      */
     public function forum(Forum $forum, Request $request): Response
     {
