a1f64aae523878b9fed16375a9ddb3b0d98cd912
faten21 <f@f.f>
2021-06-24 12:20:20 +1200

    Ajout pages erreur 404

--- public/js/error404.js
+++
@@ -1,9 +0,0 @@
-var showText = function (target, message, index, interval) {    
-    if (index < message.length) { 
-      $(target).append(message[index++]); 
-      setTimeout(function () { showText(target, message, index, interval); }, interval); 
-    } 
-  }
-  $(function () { 
-    showText("h1", "Erreur 404 : Game Over! Page non trouvée.", 0, 80);    
-  }); 
