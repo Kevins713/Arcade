b80c7447edd901d9f8ebc19384c3f0c4e087b8e7
Kevins713 <68874726+Kevins713@users.noreply.github.com>
2021-06-26 15:26:16 +0200

    Modification du front pour la partie Event, ajout du champ Image dans la modification d'une catégorie et ajout d'un script dans la page d'accueil pour récuperer l'url de la page pour les requête Ajax

--- public/js/index.js
+++ public/js/index.js
@@ -41,21 +41,49 @@ let requestStats = function(url, classSelector) {
 let requestApis = function () {
 
 
-    // Requête ajax pour récuperer les utilisateurs connectés
-    requestUsers('http://127.0.0.1:8000/api/liste-utilisateurs-connectes', 'users-connected');
+    // Requête ajax pour récuperer les utilisateurs connectés et les insérer dans l'aside
+    requestUsers( url + 'api/liste-utilisateurs-connectes', 'users-connected');
 
-// Requête pour récupérer les admins connectés
-    requestUsers('http://127.0.0.1:8000/api/liste-admins-connectes', 'admins-connected');
+// Requête pour récupérer les admins connectés et les insérer dans l'aside
+    requestUsers(url + 'api/liste-admins-connectes', 'admins-connected');
 
+// Requête pour récupérer le nombre de Forums et les insérer dans l'aside
+    requestStats(url + 'api/nombre-forums', 'forums-number');
 
-// Requête pour récupérer le nombre de Forums
-    requestStats('http://127.0.0.1:8000/api/nombre-forums', 'forums-number');
+// Requête pour récupérer le nombre de messages et les insérer dans l'aside
+    requestStats(url + 'api/nombre-messages', 'comments-number');
 
-// Requête pour récupérer le nombre de messages
-    requestStats('http://127.0.0.1:8000/api/nombre-messages', 'comments-number');
+// Requête pour récupérer le nombre de messages et les insérer dans l'aside
+    requestStats(url + 'api/nombre-utlisateurs', 'users-number');
 
-// Requête pour récupérer le nombre de messages
-    requestStats('http://127.0.0.1:8000/api/nombre-utlisateurs', 'users-number');
+    // Requête pour récupérer les 5 derniers forums
+    $.ajax({
+        type: 'GET',
+        url:  + 'api/derniers-messages',
+        dataType: 'json',
+        timeout: '7500',
+        error: function(){
+            console.log('error');
+        },
+        success: function(data){
+            $('.aside-content').text('');
+            data.forEach((forum) => {
+                console.log(forum);
+                // $('.' + classSelector).append('<span><a href="127.0.0.1:8000/profil/' + pseudonym.pseudonym +'"> ' + pseudonym.pseudonym + '</span></a>');
+                // $('.' + classSelector).append(`
+                //     <div class="row">
+                //         <div class="col-3 aside avatar d-flex align-items-center">
+                //             <img src="` + asset + '/images/usersAvatar/' + forum. + `) }}" alt="">
+                //         </div>
+                //         <div class="col-9 aside">
+                //             <h6>Aled</h6>
+                //             <p>Je sais pas quoi faire pour faire...</p>
+                //         </div>
+                //     </div>
+                // `)
+            });
+        },
+    })
 
     // Délai de 30sec avant de rafraîchir les infos
     setTimeout(requestApis, 30000);
